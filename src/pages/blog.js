6973539ab90e6ffc36d78635206eb1e97bbe29ea
markswinimer <markswinimer@gmail.com>
2020-06-08 13:56:42 -0400

    troubleshooting

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 
-import { StyledBlog, BlogContainer, BlogPostPreview } from './blog-site/Blog.styled';
+export const StyledBlog = styled.div`
+    border: .5em solid ${props => props.theme.color.navy};
+    border-bottom: none;
+    border-top: none;
+    height: 100vh;
+`
 
+export const BlogContainer = styled.div`
+    margin: auto;
+    padding-top: 3em;
+    max-width: ${props => props.theme.screen.maxWidth};
+`
+
+export const BlogPostPreview = styled.div`
+    margin-top: 2em;
+
+    h2 {
+        margin-top: .25em;
+        font-size: 24px;
+    }
+    h3 {
+        margin-top: .25em;
+        color: #6f6f6f;
+    }
+    h4 {
+        color: #EB6841;
+        font-weight: 700;
+    }
+`
 const BlogPage = ({ data }) => {
     const { edges: posts } = data.allMarkdownRemark;
     console.log(posts);
